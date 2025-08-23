4abf53de2262e6f94363083b56829737010f9235
Aldiery Rene Gonzalez <aldieryrgon@gmail.com>
2025-08-23 12:59:17 -0700

    chore: clean up project by removing all files and dependencies

--- convex/convex.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineApp } from "convex/server";
-import agent from "@convex-dev/agent/convex.config";
-import actionCache from "@convex-dev/action-cache/convex.config";
-
-const app = defineApp();
-app.use(agent);
-app.use(actionCache);
-
-export default app;
